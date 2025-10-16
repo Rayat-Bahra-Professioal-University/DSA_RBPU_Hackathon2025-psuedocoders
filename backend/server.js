2e3838a3047db36a506f6debbe3f21c6e1615c1a
Ramandeep Bhatia <bhattiramandeep3105@gmail.com>
2025-10-16 04:53:53 +0000

    backend done

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
+// backend/server.js
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const path = require('path');
 const connectDB = require('./config/db');
 
-// Import route files
-const userRoutes = require('./routes/userRoutes');
-const issueRoutes = require('./routes/issueRoutes'); // <-- IMPORT NEW ROUTES
+const authRoutes = require('./routes/auth');
+const issueRoutes = require('./routes/issueRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 dotenv.config();
 connectDB();
@@ -19,9 +21,11 @@ app.get('/', (req, res) => {
   res.json({ message: "Welcome to the CityCare API! 🏙️" });
 });
 
-// Use the routes
-app.use('/api/users', userRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/issues', issueRoutes);
+app.use('/api/comments', commentRoutes);
+app.use('/api/admin', adminRoutes);
+
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 const PORT = process.env.PORT || 5000;
